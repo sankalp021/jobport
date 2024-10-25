f2033b0a2a807c3fc4c8bd9d92b5283d4578ebed
Sankalp Chaturvedi <snklp21@gmail.com>
2024-10-25 13:37:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-
+import bodyParser = require('body-parser');
 dotenv.config({});
 
 const app = express();
@@ -16,13 +16,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
 
-app.use(cors(corsOptions));
-
+app.use(
+    cors({
+        origin :"*",
+        methods : "GET,HEAD,POST,PATCH,POST,DELETE",
+        credentials : true,
+    })
+)
+app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 
 
